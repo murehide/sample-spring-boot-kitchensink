26bd00ec9e71fb8a5194fe1c1427eb4ac6598002
murehide <66351381+murehide@users.noreply.github.com>
2020-06-05 22:22:05 +0800

    Update KitchenSinkController.java

--- src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -614,8 +614,7 @@ public class KitchenSinkController {
                                         new URIAction("立即前往",
                                                       URI.create("https://card.rakuten.com.tw/corp/campaign/cpn.xhtml?code=1124"), null)
                                 ))
-                        ));
-                carouselTemplate.CarouselTemplateBuilder.imageSize("contain");
+                        )).builder().imageSize("contain").build();
                 TemplateMessage templateMessage = new TemplateMessage("Carousel alt text", carouselTemplate);
                 this.reply(replyToken, templateMessage);
                 break;
