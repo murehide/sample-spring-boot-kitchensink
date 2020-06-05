8b7ae9a31a41c2534b8545b53cc6af065d16f62b
murehide <66351381+murehide@users.noreply.github.com>
2020-06-05 16:39:57 +0800

    Update KitchenSinkController.java

--- src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -618,7 +618,7 @@ public class KitchenSinkController {
             case "installment": {
                 CarouselTemplate carouselTemplate = new CarouselTemplate(
                         Arrays.asList(
-                                new CarouselColumn(createUri("/static/icon/1123.jpg"), "【抽Dyson】夏天到了！抽Dyson空氣清淨機，再享5%回饋", "活動期間內登錄，申辦單筆消費分期6期以上，享5%刷卡金回饋，每戶最高可回饋1,500元刷卡金，再抽Dyson空氣清淨機，筆數越多，中獎機率越高！", Arrays.asList(
+                                new CarouselColumn(createUri("/static/icon/1123.jpg"), "【抽Dyson】夏天到了！抽Dyson空氣清淨機，再享5%回饋", "申辦單筆消費分期6期以上，享5%刷卡金回饋，每戶最高可回饋1,500元刷卡金，再抽Dyson空氣清淨機，筆數越多，中獎機率越高！", Arrays.asList(
                                         new URIAction("立即前往",
                                                       URI.create("https://card.rakuten.com.tw/members/campaign/cpn.xhtml?code=1123"), null)
                                 )),
@@ -638,15 +638,15 @@ public class KitchenSinkController {
             case "japan": {
                 CarouselTemplate carouselTemplate = new CarouselTemplate(
                         Arrays.asList(
-                                new CarouselColumn(createUri("/static/icon/1059.jpg"), "逛日本MITSUI OUTLET PARK名牌輕鬆購！送購物優惠券及精美小禮!", "卡友於日本全國MITSUI OUTLET PARK消費，出示兌換券及台灣樂天信用卡,即可換取精美小禮及合作店家所提供的優惠券。", Arrays.asList(
+                                new CarouselColumn(createUri("/static/icon/1059.jpg"), "逛日本MITSUI OUTLET PARK名牌輕鬆購！送購物優惠券及精美小禮!", "出示兌換券及台灣樂天信用卡,即可換取精美小禮及合作店家所提供的優惠券。", Arrays.asList(
                                         new URIAction("立即前往",
                                                       URI.create("https://card.rakuten.com.tw/corp/japan-benefit/store.xhtml?code=1059"), null)
                                 )),
-                                new CarouselColumn(createUri("/static/icon/1087.jpg"), "BicCamera集團購物享最高免稅10%+7%OFF", "卡友於日本BicCamera集團購物，出示優惠券並刷台灣樂天信用卡，得享最高免稅10%+7%OFF。", Arrays.asList(
+                                new CarouselColumn(createUri("/static/icon/1087.jpg"), "BicCamera集團購物享最高免稅10%+7%OFF", "出示優惠券並刷台灣樂天信用卡，得享最高免稅10%+7%OFF。", Arrays.asList(
                                         new URIAction("立即前往",
                                                       URI.create("https://card.rakuten.com.tw/corp/japan-benefit/store.xhtml?code=1087"), null)
                                 )),
-                                new CarouselColumn(createUri("/static/icon/972.jpg"), "松本清免稅門市最高享免稅10%+7％OFF!", "卡友於日本松本清Matsumoto KiYoshi藥妝集團實體免稅門市購物，消費滿額享免稅10%+最高7%OFF。", Arrays.asList(
+                                new CarouselColumn(createUri("/static/icon/972.jpg"), "松本清免稅門市最高享免稅10%+7％OFF!", "實體免稅門市購物，消費滿額享免稅10%+最高7%OFF。", Arrays.asList(
                                         new URIAction("立即前往",
                                                       URI.create("https://card.rakuten.com.tw/corp/japan-benefit/store.xhtml?code=972"), null)
                                 ))
