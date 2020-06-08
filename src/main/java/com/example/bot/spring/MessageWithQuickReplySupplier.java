240d9e39cacd1f985d2936ec06a511e30ef10c9c
murehide <66351381+murehide@users.noreply.github.com>
2020-06-08 15:03:42 +0800

    Update MessageWithQuickReplySupplier.java

--- src/main/java/com/example/bot/spring/MessageWithQuickReplySupplier.java
+++ src/main/java/com/example/bot/spring/MessageWithQuickReplySupplier.java
@@ -25,6 +25,7 @@ import com.linecorp.bot.model.action.CameraRollAction;
 import com.linecorp.bot.model.action.LocationAction;
 import com.linecorp.bot.model.action.MessageAction;
 import com.linecorp.bot.model.action.PostbackAction;
+import com.linecorp.bot.model.action.URIAction;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.message.quickreply.QuickReply;
