8d7d10e161667aba9d747f87853349fdd7f4a392
murehide <66351381+murehide@users.noreply.github.com>
2020-09-04 14:54:55 +0800

    Update KitchenSinkController.java

--- src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -22,6 +22,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.StringReader;
 import java.io.UncheckedIOException;
 import java.net.URI;
 import java.net.URL;
